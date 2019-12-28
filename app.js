d69117de51eb93774dfa50465cc7ffc425beeee5
Will Corcoran <corcoranwill@gmail.com>
2019-12-27 18:23:23 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = todo => {
+  const html = `   <li
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span>${todo}</span>
+          <i class="far fa-trash-alt delete"></i>
+        </li>`;
+
+  list.innerHTML += html;
+};
+
+// filter todos event
+const filterTodos = term => {
+  // add filtered class
+  Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  // remove filtered class
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+
+// add todos event
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// delete todos event
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+});
+
+//
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
